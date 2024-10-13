29db5e5eecda2e1b07e59f085c5ae82b9639683e
Kade <kadeshikaal@gmail.com>
2024-10-13 20:33:34 +0200

    feat: added firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:chatroom_app/config/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'config/routes.dart';
 import 'blocs/theme/theme_bloc.dart';
 import 'blocs/theme/theme_state.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 
 void main() async {
   // Ensure that plugin services are initialized
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   
   // Initialize SharedPreferences
   final prefs = await SharedPreferences.getInstance();
